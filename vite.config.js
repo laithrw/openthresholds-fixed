fdb29858c45bf0d93675a3cb46ae711d46bc472a
Laith Weinberger <lweinberger24@gds.org>
2024-10-23 10:20:48 -0400

    attempting to fix links 2

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	base: '/',
-	server: {
-		historyApiFallback: true,
-	},
 	build: {
 		rollupOptions: {
-			output: {
-				manualChunks: undefined,
+			input: {
+				main: './index.html',
 			},
 		},
+		outDir: 'dist',
+		assetsDir: 'assets',
+		emptyOutDir: true,
 	},
 })
